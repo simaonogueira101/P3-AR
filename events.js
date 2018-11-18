d2b9c5fbdc27d8916192c080fea8df7a49dbf558
simaonogueira101 <simao.nogueira66@gmail.com>
2018-11-18 17:48:08 +0000

    other scenes.

--- events.js
+++ events.js
@@ -3,7 +3,6 @@ AFRAME.registerComponent('markerhandler', {
         const animatedMarker = document.querySelector("#animated-marker");
         const aEntity = document.querySelector("#model");
 
-        // every click, we make our model grow in size :)
         animatedMarker.addEventListener('click', function(ev, target){
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
             if (aEntity && intersectedElement === aEntity) {
@@ -15,20 +14,34 @@ AFRAME.registerComponent('markerhandler', {
         });
 }});
 
-
 AFRAME.registerComponent('markerhandler2', {
     init: function() {
-        // 2
-        const animatedMarker2 = document.querySelector("#animated-marker2");
-        const aEntity2 = document.querySelector("#model2");
+        const animatedMarker = document.querySelector("#animated-marker2");
+        const aEntity = document.querySelector("#model2");
 
-        animatedMarker2.addEventListener('click', function(ev2, target2){
-            const intersectedElement2 = ev2 && ev2.detail && ev2.detail.intersectedEl;
-            if (aEntity2 && intersectedElement2 === aEntity2) {
+        animatedMarker.addEventListener('click', function(ev, target){
+            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+            if (aEntity && intersectedElement === aEntity) {
                 console.log("Marker 2 detected.");
-                window.location.href = "http://google.com";
+                window.location.href = "http://stackoverflow.com";
             } else {
-              "Marker not detected 2."
+              "Marker 2 not detected."
+            }
+        });
+}});
+
+AFRAME.registerComponent('markerhandler3', {
+    init: function() {
+        const animatedMarker = document.querySelector("#animated-marker3");
+        const aEntity = document.querySelector("#model3");
+
+        animatedMarker.addEventListener('click', function(ev, target){
+            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+            if (aEntity && intersectedElement === aEntity) {
+                console.log("Marker 3 detected.");
+                window.location.href = "http://stackoverflow.com";
+            } else {
+              "Marker 3 not detected."
             }
         });
 }});
